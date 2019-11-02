dd1ea360974551f44a3e95fe05d9c11b4292d01b
ibulavsky <ibulavsky@gmail.com>
2019-11-02 23:03:29 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+In the project directory, you can run:
+### `yarn start`
+Project will be open [http://localhost:3000](http://localhost:3000) in the browser.
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            TEST REACT APP
+        </div>
+    );
+}
+export default App;
