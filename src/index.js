fab6a161d825e63ee02220ad91a5ea0d5af969db
ibulavsky <ibulavsky@gmail.com>
2019-11-29 23:25:10 +0300

    Fixed gh routing(added HashRouter)

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import store from "./BLL/redux";
 import {Provider} from "react-redux";
 
-ReactDOM.render(<BrowserRouter basename={process.env.PUBLIC_URL}>
+ReactDOM.render(<HashRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
         <App/>
     </Provider>
-</BrowserRouter>, document.getElementById('root'));
+</HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
